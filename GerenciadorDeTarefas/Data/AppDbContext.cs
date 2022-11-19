bc74005ab5795bcaea7690f30e899c5776680a62
Elexandro Torres <elexandro.torres@gmail.com>
2022-11-19 14:55:21 -0300

    Todo model and database configuration

---
+++ GerenciadorDeTarefas/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GerenciadorDeTarefas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorDeTarefas.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Todo> Tasks { get; set; }
+
+        
+
+    }
+}
---
+++ GerenciadorDeTarefas/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace GerenciadorDeTarefas.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DateOfConclusion { get; set; } = DateTime.Now;
+
+    }
+}
